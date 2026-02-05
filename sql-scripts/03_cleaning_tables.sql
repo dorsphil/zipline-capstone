247498d81aae4addd3078a4d4b01efadac34c83c
dorsphil <dorsphilloseiasuming@gmail.com>
2026-02-05 23:14:16 +0000

    condensed most of the query statements

--- sql-scripts/03_cleaning_tables.sql
+++ sql-scripts/03_cleaning_tables.sql
@@ -26,62 +26,35 @@ UPDATE facilities SET
 
 
 --	for deliveries table ---
-SELECT * FROM deliveries WHERE NEST_NAME IS NULL OR TRIM(NEST_NAME) = '';
-
-SELECT * FROM deliveries WHERE PRIORITY IS NULL OR TRIM(PRIORITY) = '';
-
-SELECT * FROM deliveries WHERE DELIVERY_KEY IS NULL OR TRIM(DELIVERY_KEY) = '';
-
-SELECT * FROM deliveries WHERE HEALTH_FACILITY_NAME IS NULL OR TRIM(HEALTH_FACILITY_NAME) = '';
-
-SELECT * FROM deliveries WHERE HEALTH_FACILITY_LOCALITY IS NULL OR TRIM(HEALTH_FACILITY_LOCALITY) = '';
-
-SELECT * FROM deliveries WHERE N_UNITS IS NULL;
-
-SELECT * FROM deliveries WHERE PRODUCT_NAME IS NULL OR TRIM(PRODUCT_NAME) = '';
-
-SELECT * FROM deliveries WHERE USE_CASE_SUBCATEGORY IS NULL OR TRIM(USE_CASE_SUBCATEGORY) = '';
-
-SELECT * FROM deliveries WHERE USE_CASE_CATEGORY IS NULL OR TRIM(USE_CASE_CATEGORY) = '';
-
-SELECT * FROM deliveries WHERE TIME_ORDER_CONFIRMED_LOCAL IS NULL;
-
-SELECT * FROM deliveries WHERE TIME_DELIVERED_LOCAL IS NULL;
+SELECT * FROM deliveries 
+WHERE NEST_NAME IS NULL OR TRIM(NEST_NAME) = ''
+   OR PRIORITY IS NULL OR TRIM(PRIORITY) = ''
+   OR DELIVERY_KEY IS NULL OR TRIM(DELIVERY_KEY) = ''
+   OR HEALTH_FACILITY_NAME IS NULL OR TRIM(HEALTH_FACILITY_NAME) = ''
+   OR N_UNITS IS NULL
+   OR TIME_ORDER_CONFIRMED_LOCAL IS NULL;
 
 
 -- for facilities table ---
-SELECT * FROM facilities WHERE FACILITY_ID IS NULL;
-
-SELECT * FROM facilities WHERE FACILITY_NAME IS NULL OR TRIM(FACILITY_NAME) = '';
-
-SELECT * FROM facilities WHERE FACILITY_TYPE IS NULL OR TRIM(FACILITY_TYPE) = '';
-
-SELECT * FROM facilities WHERE DISTRICT IS NULL OR TRIM(DISTRICT) = '';
-
-SELECT * FROM facilities WHERE REGION IS NULL OR TRIM(REGION) = '';
-
-SELECT * FROM facilities WHERE SITE_ID IS NULL OR SITE_ID = '';
+SELECT * FROM facilities 
+WHERE FACILITY_NAME IS NULL OR TRIM(FACILITY_NAME) = ''
+   OR DISTRICT IS NULL OR TRIM(DISTRICT) = ''
+   OR REGION IS NULL OR TRIM(REGION) = ''
+   OR SITE_ID IS NULL OR SITE_ID = 0;
 
 
 --------------------------------------------
 --	   fix null/empty values
 --------------------------------------------
-- null/empty values were found in only 3 columns of facilities table
+-- null/empty values were found in only 3 columns of facilities table
 
 -- fix for DISTRICT column(5rows) 
+-- Fix District and Region in one table update
 UPDATE facilities 
-     SET DISTRICT = 'Unknown'
-     WHERE TRIM(DISTRICT) = '' OR DISTRICT IS NULL;
-
--- fix for REGION column(5rows)
-UPDATE facilities
-    SET REGION = 'Unknown Region'
-    WHERE TRIM(REGION) = '' OR REGION IS NULL;
-
--- fix for SITE_ID column(59rows)
-UPDATE facilities
-    SET SITE_ID = NULL
-    WHERE SITE_ID = 0;
+SET 
+    DISTRICT = CASE WHEN (TRIM(DISTRICT) = '' OR DISTRICT IS NULL) THEN 'Unknown' ELSE DISTRICT END,
+    REGION = CASE WHEN (TRIM(REGION) = '' OR REGION IS NULL) THEN 'Unknown Region' ELSE REGION END,
+    SITE_ID = CASE WHEN SITE_ID = 0 THEN NULL ELSE SITE_ID END;
 
 /*
  * Update the single facility identified as having an active service 
@@ -99,7 +72,7 @@ UPDATE facilities
 SELECT FACILITY_NAME, DISTRICT,
        REGION, SITE_ID
        FROM  facilities
-       WHERE FACILITY_NAME = 'Asawisno SDA HSP' AND SITE ID IS NULL; --since all blanks were set to NULL initially
+       WHERE FACILITY_NAME = 'Asawinso SDA HSP' AND SITE_ID IS NULL; --since all blanks were set to NULL initially
 
 -- update 'SITE_ID' as found from Controller Team --
 UPDATE facilities
@@ -226,7 +199,7 @@ SELECT *
 --  make up 0.33% of the entire dataset hence insignificant
 SELECT *
 	FROM deliveries
-	WHERE TIME_DELIVERED_LOCAL < TIME_ORDER_CONFIRMED LOCAL;
+	WHERE TIME_DELIVERED_LOCAL < TIME_ORDER_CONFIRMED_LOCAL;
 
 -- create a view to exclude incorrect times
 -- add a delivery duration column
